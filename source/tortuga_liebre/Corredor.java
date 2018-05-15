4f933a591e7da9bcb9c5afb3a8ea52381fe8dde6
Unknown <fofonso_97@me.com>
2018-05-15 11:52:34 -0600

    Se refactoriza clase corredor

--- source/tortuga_liebre/Corredor.java
+++ source/tortuga_liebre/Corredor.java
@@ -1,11 +1,20 @@
 package tortuga_liebre;
 
+import java.awt.image.BufferedImage;
+
 public class Corredor extends Thread {
 	
 	private int corredorID;
+	private BufferedImage imagenCorredor;
+	private boolean corriendo = true;
+	private int distanciaRecorrida = 0;
+	private Semaforo[] semaforos;
+	private boolean[] puentes;
 
-	public Corredor(int corredorID) {
+	public Corredor(int corredorID, Semaforo[] semaforos, boolean[] puentes) {
 		this.corredorID = corredorID;
+		this.semaforos = semaforos;
+		this.puentes = puentes;
 	}
 
 	public int getCorredorID() {
@@ -14,6 +23,53 @@ public class Corredor extends Thread {
 
 	public void setCorredorID(int corredorID) {
 		this.corredorID = corredorID;
+	}
+
+	public Semaforo[] getSemaforos() {
+		return semaforos;
+	}
+
+	public void setSemaforos(Semaforo[] semaforos) {
+		this.semaforos = semaforos;
+	}
+
+	public boolean[] getPuentes() {
+		return puentes;
+	}
+
+	public void setPuentes(boolean[] puentes) {
+		this.puentes = puentes;
+	}
+
+	public BufferedImage getImagenCorredor() {
+		return imagenCorredor;
+	}
+
+	public void setImagenCorredor(BufferedImage imagenCorredor) {
+		this.imagenCorredor = imagenCorredor;
+	}
+
+	public int getDistanciaRecorrida() {
+		return distanciaRecorrida;
+	}
+
+	public void setDistanciaRecorrida(int distanciaRecorrida) {
+		this.distanciaRecorrida = distanciaRecorrida;
+	}
+	
+	public boolean estaCorriendo() {
+		return corriendo;
+	}
+
+	public void setCorriendo(boolean corriendo) {
+		this.corriendo = corriendo;
+	}
+
+	public boolean enPuente(int posicion) {
+		if(posicion >= 30 && posicion <= 40) {
+			return true;
+		}
+		return false;	
 	}
 	
 }
